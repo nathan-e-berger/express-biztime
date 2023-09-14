d71b9ffeb663c5d18337a164e5f6244de6c781d3
Gabe Yoro <gabrielyoro@gmail.com>
2023-09-14 16:43:14 -0700

    Refactor post-code-review

--- routes/companies.js
+++ routes/companies.js
@@ -14,7 +14,8 @@ const { validatePost, validatePut } = require("../utils");
 router.get("/", async function (req, res) {
     const results = await db.query(
         `SELECT *
-        FROM companies`
+        FROM companies
+        ORDER BY code`
     );
     const companies = results.rows;
     return res.json({ companies });
@@ -25,11 +26,14 @@ router.get("/", async function (req, res) {
  * Return : {company: {code, name, description}}
  */
 router.get("/:code", async function (req, res) {
-    const { code } = req.params;
+    const code = req.params.code;
 
-    const results = await db.query(`SELECT * FROM companies WHERE code = $1`, [
-        code,
-    ]);
+    const results = await db.query(
+        `
+        SELECT * FROM companies 
+        WHERE code = $1`,
+        [code]
+    );
 
     const company = results.rows[0];
 
@@ -47,12 +51,13 @@ router.post("/", validatePost, async function (req, res) {
     const { code, name, description } = req.body;
 
     const results = await db.query(
-        `INSERT INTO companies(code, name, description) VALUES($1, $2, $3) RETURNING code, name, description`,
+        `INSERT INTO companies(code, name, description) 
+        VALUES($1, $2, $3) RETURNING code, name, description`,
         [code, name, description]
     );
 
     const company = results.rows[0];
-    return res.json({ company });
+    return res.status(201).json({ company });
 });
 
 /**
@@ -62,13 +67,13 @@ router.post("/", validatePost, async function (req, res) {
  */
 router.put("/:code", validatePut, async function (req, res) {
     const { name, description } = req.body;
-    const { code } = req.params;
+    const code = req.params.code;
 
     const results = await db.query(
-        `UPDATE companies SET name=$2, description=$3
-      WHERE code = $1
+        `UPDATE companies SET name=$1, description=$2
+      WHERE code = $3
         RETURNING code, name, description`,
-        [code, name, description]
+        [name, description, code]
     );
     if (!results.rows.length)
         throw new BadRequestError("There doesnt seem to be anything here");
@@ -82,15 +87,17 @@ router.put("/:code", validatePut, async function (req, res) {
  * Returns : { {status: "deleted"} }
  */
 router.delete("/:code", async function (req, res) {
+    // remove
     const { code } = req.params;
 
     const results = await db.query(
         `DELETE FROM companies
-      WHERE code = $1
+        WHERE code = $1
         RETURNING code`,
         [code]
     );
-
+    // throw 404
+    if (!results.rows[0]) throw new NotFoundError("Company not found");
     return res.json({ status: "deleted" });
 });
 
